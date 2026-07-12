a004315dbebf70591739b3e9e7a3376efcbd5138
zwj <2839056693@qq.com>
2026-07-12 21:14:06 +0800

    反转链表 迭代

--- src/test20260707/ReverseList.java
+++ src/test20260707/ReverseList.java
@@ -1,9 +1,10 @@
 package test20260707;
 
+import java.awt.*;
 import java.util.Stack;
 
 /**
- * 反转链表
+ * 反转链表 --- ①最简单想法 用一个栈 ②迭代 ③递归
  */
 public class ReverseList {
     public static void main(String[] args) {
@@ -27,5 +28,21 @@ public class ReverseList {
         // 关键：末尾节点next置空，切断循环
         res.next = null;
         return dummy;
+    }
+    public static ListNode reverseList1(ListNode head){
+        if(head==null){
+            return null;
+        }else if(head.next==null){
+            return head;
+        }
+        ListNode cur = head;
+        ListNode pre = null;
+        while (cur!=null){
+            ListNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
     }
 }
